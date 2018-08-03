b60bd274e52e46d7ccba8de638aedfef9a4eb6e0
Alexey <a.ilyin@id-sys.ru>
2018-08-03 16:51:52 +0300

    Исправлено 3-е задание. Сделан один передаваемый параметр

--- sandbox/src/test/java/ru/stqa/pft/sandbox/DistancePointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/DistancePointTest.java
@@ -15,14 +15,11 @@ public class DistancePointTest {
     Point p6 = new Point(23, 45);
     //Объявление координат для точек.
 
-    Point p = new Point(0,0);
-    //Создания обьекта типа Point для наглядного вызова метода соответствующего класса. Передаем параметры (0, 0), так как объект должен получить любые 2 атрибута на вход
-
-    Assert.assertEquals(p.distanceMetod(p1, p2), 50.0);
-    Assert.assertEquals(p.distanceMetod(p3, p4), 20.0);
-    Assert.assertEquals(p.distanceMetod(p5, p6), 34.058772731852805);
-    Assert.assertEquals(p.distanceMetod(p1, p4), 0.0);
-    Assert.assertEquals(p.distanceMetod(p3, p6), 33.97057550292606);
+    Assert.assertEquals(p1.distanceMetod(p2), 50.0);
+    Assert.assertEquals(p3.distanceMetod(p4), 20.0);
+    Assert.assertEquals(p5.distanceMetod(p6), 34.058772731852805);
+    Assert.assertEquals(p1.distanceMetod(p4), 0.0);
+    Assert.assertEquals(p3.distanceMetod(p6), 33.97057550292606);
 
   }
 }
