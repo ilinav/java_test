efdbe55293d6da7b6c787cf9d5e517bbc92fcf3e
Alexey <a.ilyin@id-sys.ru>
2018-08-10 13:46:12 +0300

    Ренейм класса как и в лекциях

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
 
-public class GroupCreateTests {
+public class GroupCreationTests {
   FirefoxDriver wd;
 
   @BeforeMethod
