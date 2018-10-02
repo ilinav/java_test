9208c8fdd84447aaf58892837fefc662c6baefdf
Alexey <a.ilyin@id-sys.ru>
2018-10-02 16:45:53 +0300

    Глобальный запуск браузера для тестов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AllContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AllContactDeletionTests.java
@@ -1,17 +1,24 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.FioName;
+
+import java.util.List;
 
 public class AllContactDeletionTests extends TestBase {
 
-  @Test
+  @Test(enabled = false)
   public void testAllContactDeletion() {
     //удаление всех контактов в addressbook через страницу home с окном подтверждения
     app.getNavigationHelper().gotoHomePage();
+    List<FioName> before = app.getContactHelper().getContactList();
     app.getContactHelper().selectAllContact();
     app.getContactHelper().deleteAllSelectedContacts();
     app.getContactHelper().submitContactsDeletion();
     app.getNavigationHelper().gotoHomePage();
+    List<FioName> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), 0);
   }
 }
 
